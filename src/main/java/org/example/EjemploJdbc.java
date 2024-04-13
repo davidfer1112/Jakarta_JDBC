e5ca6e6aa1f945f70c6eae0efbb8eba32bd405d5
davidfer1112 <davidfernando1112@gmail.com>
2024-04-13 18:49:53 -0500

    Feature: repository Categoria implementacion

--- src/main/java/org/example/EjemploJdbc.java
+++ src/main/java/org/example/EjemploJdbc.java
@@ -10,7 +10,7 @@ import java.sql.*;
 
 
 public class EjemploJdbc {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
 
             Repository<Producto> repository = new ProductoRepositoryImpl();
---
+++ src/main/java/org/example/repository/CategoriaRepositoryImp.java
@@ -0,0 +1,78 @@
+package org.example.repository;
+
+import org.example.models.Categoria;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaRepositoryImp implements Repository<Categoria>{
+
+    private Connection connection;
+
+    @Override
+    public List<Categoria> listar() throws SQLException {
+        List<Categoria> categorias = new ArrayList<>();
+        try(Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM categorias")){
+            while (rs.next()){
+                categorias.add(crearCategoria(rs));
+            }
+        }
+        return categorias;
+    }
+
+    @Override
+    public Categoria porId(Long id) throws SQLException {
+        Categoria categoria = null;
+        try(PreparedStatement stmt = connection.prepareStatement("SELECT * FROM categorias as c WHERE c.id = ?")){
+            stmt.setLong(1, id);
+            try(ResultSet rs = stmt.executeQuery()){
+                if(rs.next()){
+                    categoria = crearCategoria(rs);
+                }
+            }
+        }
+        return categoria;
+    }
+
+    @Override
+    public Categoria guardar(Categoria categoria) throws SQLException {
+        String sql = null;
+        if (categoria.getId() != null && categoria.getId() > 0) {
+            sql = "UPDATE categorias SET nombre = ? WHERE id = ?";
+        } else {
+            sql = "INSERT INTO categorias (nombre) VALUES (?)";
+        }
+        try(PreparedStatement stmt = connection.prepareStatement(sql, Statement.NO_GENERATED_KEYS)){
+            stmt.setString(1, categoria.getNombre());
+            if (categoria.getId() != null && categoria.getId() > 0) {
+                stmt.setLong(2, categoria.getId());
+            }
+            stmt.executeUpdate();
+            if (categoria.getId() == null){
+                try(ResultSet rs = stmt.getGeneratedKeys()){
+                    if(rs.next()){
+                        categoria.setId(rs.getLong(1));
+                    }
+                }
+            }
+        }
+        return categoria;
+    }
+
+    @Override
+    public void eliminar(Long id) throws SQLException {
+        try(PreparedStatement stmt = connection.prepareStatement("DELETE FROM categorias WHERE id = ?")){
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+        }
+    }
+
+    private static Categoria crearCategoria(ResultSet rs) throws SQLException {
+        Categoria c = new Categoria();
+        c.setId(rs.getLong("id"));
+        c.setNombre(rs.getString("nombre"));
+        return c;
+    }
+}
