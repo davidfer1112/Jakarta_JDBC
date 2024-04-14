d6a8e44c44fc143150dcb3284f80eb74feaeb4ee
davidfer1112 <davidfernando1112@gmail.com>
2024-04-13 22:02:48 -0500

    Feature implementacion service

--- src/main/java/org/example/EjemploJdbc.java
+++ src/main/java/org/example/EjemploJdbc.java
@@ -1,11 +1,15 @@
 package org.example;
+
 import org.example.models.Categoria;
 import org.example.models.Producto;
 import org.example.repository.ProductoRepositoryImpl;
 import org.example.repository.CategoriaRepositoryImp;
 import org.example.repository.Repository;
+import org.example.service.CatalogoServicio;
+import org.example.service.Service;
 import org.example.util.ConexionBaseDatos;
-import  java.util.Date;
+
+import java.util.Date;
 
 import java.sql.*;
 
@@ -13,47 +17,28 @@ import java.sql.*;
 public class EjemploJdbc {
     public static void main(String[] args) throws SQLException {
 
-            try(Connection connection = ConexionBaseDatos.getConnection()) {
-                    if (connection.getAutoCommit()) {
-                        connection.setAutoCommit(false);
-                    }
-                    try {
-                            Repository<Categoria> repositoryCategoria = new CategoriaRepositoryImp(connection);
-                            System.out.println("Insertando una nueva categoria");
-                            Categoria categoria = new Categoria();
-                            categoria.setNombre("Hogar");
-                            Categoria nuevaCategoria = repositoryCategoria.guardar(categoria);
-                            System.out.println("Categoria guardada con exito: " + nuevaCategoria.getId());
+        Service service = new CatalogoServicio();
 
-                            Repository<Producto> repository = new ProductoRepositoryImpl(connection);
-                            System.out.println("\nListado de productos:");
-                            repository.listar().forEach(System.out::println);
+        System.out.println("\nListado de productos:");
+        service.listar().forEach(System.out::println);
+        System.out.println("Insertando una nueva categoria");
+        Categoria categoria = new Categoria();
+        categoria.setNombre("Gamer");
 
-                            // Se imprime el producto con id 2 (La L es para indicar que es un Long)
-                            System.out.println("\nConsultando el producto con id 2:");
-                            System.out.println(repository.porId(2L));
 
-                            // Se inserta un nuevo producto
-                            System.out.println("\nInsertando un nuevo producto");
-                            Producto producto = new Producto();
-                            producto.setNombre("Microondas");
-                            producto.setPrecio(350.0);
-                            producto.setFechaRegistro(new Date());
-                            producto.setSku("abcdef123");
-                            producto.setCategoria(nuevaCategoria);
-                            repository.guardar(producto);
-                            System.out.println("Producto guardado con exito: " + producto.getId());
+        // Se inserta un nuevo producto
+        System.out.println("\nInsertando un nuevo producto");
+        Producto producto = new Producto();
+        producto.setNombre("Laptop Gamer");
+        producto.setPrecio(450.0);
+        producto.setFechaRegistro(new Date());
+        producto.setSku("LPG-001");
+        service.guardarProductoConCategoria(producto, categoria);
+        System.out.println("Producto guardado con exito: " + producto.getId());
 
-                            // Se imprime la lista de productos
-                            System.out.println("\nListado de productos:");
-                            repository.listar().forEach(System.out::println);
-
-                            connection.commit();
-                    }catch (SQLException e){
-                        connection.rollback();
-                        e.printStackTrace();
-                    }
-            }
+        // Se imprime la lista de productos
+        System.out.println("\nListado de productos:");
+        service.listar().forEach(System.out::println);
 
     }
 }
